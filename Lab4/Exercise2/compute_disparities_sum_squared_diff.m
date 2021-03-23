c97daabd8e09c9c3a7c1fc036f1fa40f4ba1dfaf
Bruin96 <Sjoerd.Bruin.1996@gmail.com>
2021-03-23 13:48:10 +0100

    Shifted to a dynamic programming implementation. This accelerates the code massively, roughly 10x faster, Still need to implement for Pearson.

--- Lab4/Exercise2/compute_disparities_sum_squared_diff.m
+++ Lab4/Exercise2/compute_disparities_sum_squared_diff.m
@@ -19,8 +19,49 @@ function dispars = compute_disparities_sum_squared_diff(im1, im2, win_height, wi
     end
     
     % Initialise costs matrix at a high value to facilitate minimum operation later on
+    row_vals = zeros(M, N, max_disparity+1, 'single');
     costs = 12000*ones(M, N, max_disparity+1, 'single'); 
     
+    im_beam = im1(x_bottom-pad_height:x_bottom+pad_height, :);
+    for y = y_bottom:y_top
+        im1_window = im_beam(:, y-pad_width:y+pad_width);
+        for d = lower_bound(x_bottom, y):0
+            im2_window = im2(x_bottom-pad_height:x_bottom+pad_height, y+d-pad_width:y+d+pad_width);
+            abs_diff = 0;
+            for i = x_bottom-pad_height:x_bottom+pad_height
+                row_vals(i, y, -d+1) = sum((im1_window(i, :) - im2_window(i, :)).^2);
+                abs_diff = abs_diff + row_vals(i, y, -d+1);
+            end
+            costs(x_bottom, y, -d+1) = abs_diff;
+        end
+    end
+    %curr_row_vals = row_vals(x_bottom-pad_height:x_bottom+pad_height, :, 1)
+    
+    for x = x_bottom+1:x_top
+        for y = y_bottom:y_top
+            if y <= y_bottom+max_disparity+1 % lower_bound changes in this region, so we can't use area table here
+                im1_window = im1(x+pad_height, y-pad_width:y+pad_width);
+                for d = lower_bound(x, y):0
+                    row_vals(x+pad_height, y, -d+1) = sum((im1_window - im2(x+pad_height, y+d-pad_width:y+d+pad_width)).^2);
+                    costs(x, y, -d+1) = costs(x-1, y, -d+1) - row_vals(x-1-pad_height, y, -d+1) + row_vals(x+pad_height, y, -d+1);
+                end
+            else % We use full dynamic programming here
+                for d = lower_bound(x, y):0
+                    %x
+                    %y
+                    %d
+                    costs(x, y, -d+1) = costs(x-1, y, -d+1) + costs(x, y-1, -d+1) - costs(x-1, y-1, -d+1) + ...
+                        (im1(x-1-pad_height, y-1-pad_width) - im2(x-1-pad_height, y-1+d-pad_width))^2 - ...
+                        (im1(x-1-pad_height, y+pad_width) - im2(x-1-pad_height, y+d+pad_width))^2 - ...
+                        (im1(x+pad_height, y-1-pad_width) - im2(x+pad_height, y-1+d-pad_width))^2 + ...
+                        (im1(x+pad_height, y+pad_width) - im2(x+pad_height, y+d+pad_width))^2;
+                end
+                
+            end
+        end
+    end
+    
+    %{
     for x = x_bottom:x_top
         im1_beam = reshape(im1(x-pad_height:x+pad_height, :), [beam_size, 1]);
         im2_beam = reshape(im2(x-pad_height:x+pad_height, :), [beam_size, 1]);
@@ -34,6 +75,7 @@ function dispars = compute_disparities_sum_squared_diff(im1, im2, win_height, wi
             end
         end
     end   
+    %}
     
     [~, dispar_val] = min(costs(x_bottom:x_top, y_bottom:y_top, :), [], 3);
     dispars(x_bottom:x_top, y_bottom:y_top) = dispar_val-1;
--- Lab4/Exercise2/run_exercise_2.m
+++ Lab4/Exercise2/run_exercise_2.m
@@ -10,9 +10,9 @@ im1_gpu = gpuArray(single(im1));
 im2_gpu = gpuArray(single(im2));
 
 [M, N] = size(im1);
-win_height = 23;
-win_width = 23;
-disparities = compute_disparities(im1, im2, 'AD', win_height, win_width);
+win_height = 5;
+win_width = 5;
+disparities = compute_disparities(im1, im2, 'XCORR', win_height, win_width);
 
 % Convert disparity to grey-scale
 min_val = min(min(disparities));
