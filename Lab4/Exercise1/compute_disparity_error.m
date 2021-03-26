aecc7e284f1358a8824cdf8d5be8bbfb71256b2a
Jasper van Thuijl <jaspervanthuijl@gmail.com>
2021-03-26 12:14:20 +0100

    Updated ex 1 and 4

--- Lab4/Exercise1/compute_disparity_error.m
+++ Lab4/Exercise1/compute_disparity_error.m
@@ -1,15 +1,11 @@
-function MSE = compute_disparity_error(I1,I2, ground_truth, max_disparity)
-    [N,M] = size(I1);
-    %  map = zeros(N,M);
-
-    
+function MSE = compute_disparity_error(I1,I2, ground_truth, max_disparity)  
     [x1,y1,x2,~] = pick_features(I1,I2);
     MSE = 0;
     % loop through all features
     for n = 1:length(x1)
         % value at position (floor(x1(n)), floor(y1(n)))
         prediction = abs(x2(n) - x1(n))
-        value = ground_truth(floor(x1(n)), floor(y1(n))) * max_disparity
+        value = ground_truth(floor(y1(n)), floor(x1(n))) * max_disparity
         MSE = MSE + (value - prediction)^2;
     end
-    MSE = MSE / n;
+    MSE = MSE / length(x1);
